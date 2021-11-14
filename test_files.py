f9a097417a97298049c01d603bfb392f6d1cef68
Lulu <lucas.thil@yahoo.com>
2021-11-14 16:53:22 +0200

    Update test_files.py

--- test_files.py
+++ test_files.py
@@ -2,10 +2,3 @@
 # test file for drive/io google colab functions
 
 print('lets go')
-
-from google.colab import drive
-drive.mount('/gdrive')
-
-with open('/gdrive/My Drive/Colab Notebooks/foo.txt', 'w') as f:
-  f.write('Hello Google Drive!')
-!cat '/gdrive/My Drive/Colab Notebooks/foo.txt'
