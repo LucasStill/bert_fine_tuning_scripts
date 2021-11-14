55dd39cf4898a10a4952c7f0bf2da3c9479a2c30
Lucas <lucas@quantum1net.com>
2021-11-14 15:42:41 +0100

    initial commit

---
+++ test_files.py
@@ -0,0 +1,9 @@
+# test file for drive/io google colab functions
+
+
+from google.colab import drive
+drive.mount('/gdrive')
+
+with open('/gdrive/My Drive/Colab Notebooks/foo.txt', 'w') as f:
+  f.write('Hello Google Drive!')
+!cat '/gdrive/My Drive/Colab Notebooks/foo.txt'
