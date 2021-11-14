2717710de27f4b1fc5c3d9f902d3020b3b206af0
Lulu <lucas.thil@yahoo.com>
2021-11-14 16:59:04 +0200

    Update test_files.py

--- test_files.py
+++ test_files.py
@@ -2,9 +2,27 @@
 # test file for drive/io google colab functions
 
 import logging
-
-print('lets go')
-
+  
 logger = logging.getLogger(__name__)
-logger.info('hey lets go')
-logger.warning('hey this time')
+
+
+main():
+
+  print('lets go')
+
+
+  # Setup logging
+      logging.basicConfig(
+          format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+          datefmt="%m/%d/%Y %H:%M:%S",
+          level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN,
+      )
+
+  logger.info('hey lets go')
+  logger.warning('hey this time')
+
+
+
+
+if __name__ == "__main__":
+    main()
