5f139f8d3aa14702d2e7fa1471d0343c12e4be39
Lulu <lucas.thil@yahoo.com>
2021-11-14 17:00:17 +0200

    Update test_files.py

--- test_files.py
+++ test_files.py
@@ -1,28 +1,6 @@
 # coding=utf-8
 # test file for drive/io google colab functions
-
-import logging
   
-logger = logging.getLogger(__name__)
-
-
-main():
-
-  print('lets go')
-
-
-  # Setup logging
-      logging.basicConfig(
-          format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
-          datefmt="%m/%d/%Y %H:%M:%S",
-          level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN,
-      )
-
-  logger.info('hey lets go')
-  logger.warning('hey this time')
-
-
-
-
-if __name__ == "__main__":
-    main()
+import logging
+logging.warning('Watch out!')  # will print a message to the console
+logging.info('I told you so')  # will not print anything
